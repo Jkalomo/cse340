b764958e68a30e33822cbfd0446d4407eaf41e17
Jkalomo <jkalomo@byupathway.edu>
2025-09-13 07:43:53 +0200

    Commit

--- database/assignment2.sql
+++ database/assignment2.sql
@@ -1,29 +1,33 @@
--- 1. Insert Tony Stark into the account table
-INSERT INTO account (first_name, last_name, email, password)
+-- Assignment 2 - Task 1: Six SQL Statements
+-- File: assignment2.sql
+-- Student: [Jabulani Kalomo]
+-- Date: [13 September 2025]
+
+-- Query 1: Insert Tony Stark record into account table
+INSERT INTO account (account_firstname, account_lastname, account_email, account_password)
 VALUES ('Tony', 'Stark', 'tony@starkent.com', 'Iam1ronM@n');
 
--- 2. Update Tony Stark's account_type to Admin
-UPDATE account
-SET account_type = 'Admin'
-WHERE email = 'tony@starkent.com';
+-- Query 2: Modify Tony Stark record to change account_type to "Admin"
+UPDATE account 
+SET account_type = 'Admin' 
+WHERE account_email = 'tony@starkent.com';
 
--- 3. Delete Tony Stark's record
-DELETE FROM account
-WHERE email = 'tony@starkent.com';
+-- Query 3: Delete Tony Stark record from the database
+DELETE FROM account 
+WHERE account_email = 'tony@starkent.com';
 
--- 4. Update GM Hummer description using REPLACE()
-UPDATE inventory
+-- Query 4: Modify GM Hummer description using REPLACE function
+UPDATE inventory 
 SET inv_description = REPLACE(inv_description, 'small interiors', 'a huge interior')
 WHERE inv_make = 'GM' AND inv_model = 'Hummer';
 
--- 5. Select make, model, and classification for Sport vehicles
+-- Query 5: Inner join to select make, model, and classification name for Sport category
 SELECT i.inv_make, i.inv_model, c.classification_name
 FROM inventory i
-INNER JOIN classification c
-  ON i.classification_id = c.classification_id
+INNER JOIN classification c ON i.classification_id = c.classification_id
 WHERE c.classification_name = 'Sport';
 
--- 6. Update inventory image paths to include /vehicles
-UPDATE inventory
+-- Query 6: Update all inventory records to add "/vehicles" to image paths
+UPDATE inventory 
 SET inv_image = REPLACE(inv_image, '/images/', '/images/vehicles/'),
     inv_thumbnail = REPLACE(inv_thumbnail, '/images/', '/images/vehicles/');
