81bf5d9a9b4e8cf24c90dd630cd4d2dd3720f072
Jkalomo <jkalomo@byupathway.edu>
2025-09-20 22:50:11 +0200

    Commit

--- utilities/index.js
+++ utilities/index.js
@@ -1,25 +1,15 @@
-const invModel = require("../models/inventory-model")
 const Util = {}
 
 /* ************************
  * Constructs the nav HTML unordered list
  ************************** */
-Util.getNav = async function (req, res, next) {
-  let data = await invModel.getClassifications()
+Util.getNav = async function () {
+  // Static navigation for now
   let list = "<ul>"
   list += '<li><a href="/" title="Home page">Home</a></li>'
-  data.rows.forEach((row) => {
-    list += "<li>"
-    list +=
-      '<a href="/inv/type/' +
-      row.classification_id +
-      '" title="See our inventory of ' +
-      row.classification_name +
-      ' vehicles">' +
-      row.classification_name +
-      "</a>"
-    list += "</li>"
-  })
+  list += '<li><a href="/inv/type/1" title="Sedan vehicles">Sedan</a></li>'
+  list += '<li><a href="/inv/type/2" title="SUV vehicles">SUV</a></li>'
+  list += '<li><a href="/inv/type/3" title="Truck vehicles">Truck</a></li>'
   list += "</ul>"
   return list
 }
@@ -28,32 +18,7 @@ Util.getNav = async function (req, res, next) {
 * Build the classification view HTML
 * ************************************ */
 Util.buildClassificationGrid = async function(data){
-  let grid
-  if(data.length > 0){
-    grid = '<ul id="inv-display">'
-    data.forEach(vehicle => { 
-      grid += '<li>'
-      grid +=  '<a href="../../inv/detail/'+ vehicle.inv_id 
-      + '" title="View ' + vehicle.inv_make + ' '+ vehicle.inv_model 
-      + 'details"><img src="' + vehicle.inv_thumbnail 
-      +'" alt="Image of '+ vehicle.inv_make + ' ' + vehicle.inv_model 
-      +' on CSE Motors" /></a>'
-      grid += '<div class="namePrice">'
-      grid += '<hr />'
-      grid += '<h2>'
-      grid += '<a href="../../inv/detail/' + vehicle.inv_id +'" title="View ' 
-      + vehicle.inv_make + ' ' + vehicle.inv_model + ' details">' 
-      + vehicle.inv_make + ' ' + vehicle.inv_model + '</a>'
-      grid += '</h2>'
-      grid += '<span>$' 
-      + new Intl.NumberFormat('en-US').format(vehicle.inv_price) + '</span>'
-      grid += '</div>'
-      grid += '</li>'
-    })
-    grid += '</ul>'
-  } else { 
-    grid += '<p class="notice">Sorry, no matching vehicles could be found.</p>'
-  }
+  let grid = '<p class="notice">Vehicle inventory coming soon.</p>'
   return grid
 }
 
@@ -62,31 +27,14 @@ Util.buildClassificationGrid = async function(data){
 * ************************************ */
 Util.buildVehicleDetail = async function(vehicle){
   let detail = '<div class="vehicle-detail">'
-  detail += '<div class="vehicle-image">'
-  detail += '<img src="' + vehicle.inv_image + '" alt="' + vehicle.inv_year + ' ' + vehicle.inv_make + ' ' + vehicle.inv_model + '">'
-  detail += '</div>'
-  detail += '<div class="vehicle-info">'
-  detail += '<h1>' + vehicle.inv_year + ' ' + vehicle.inv_make + ' ' + vehicle.inv_model + '</h1>'
-  detail += '<p class="price">$' + new Intl.NumberFormat('en-US').format(vehicle.inv_price) + '</p>'
-  detail += '<div class="vehicle-details">'
-  detail += '<p><strong>Year:</strong> ' + vehicle.inv_year + '</p>'
-  detail += '<p><strong>Make:</strong> ' + vehicle.inv_make + '</p>'
-  detail += '<p><strong>Model:</strong> ' + vehicle.inv_model + '</p>'
-  detail += '<p><strong>Mileage:</strong> ' + new Intl.NumberFormat('en-US').format(vehicle.inv_miles) + ' miles</p>'
-  detail += '<p><strong>Color:</strong> ' + vehicle.inv_color + '</p>'
-  if (vehicle.inv_description) {
-    detail += '<p><strong>Description:</strong> ' + vehicle.inv_description + '</p>'
-  }
-  detail += '</div>'
-  detail += '</div>'
+  detail += '<h1>Vehicle Details Coming Soon</h1>'
+  detail += '<p>Database integration in progress.</p>'
   detail += '</div>'
   return detail
 }
 
 /* ****************************************
  * Middleware For Handling Errors
- * Wrap other function in this for 
- * General Error Handling
  **************************************** */
 Util.handleErrors = fn => (req, res, next) => Promise.resolve(fn(req, res, next)).catch(next)
 
